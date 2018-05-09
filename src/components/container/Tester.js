a200437e7a930847787ca43e45a354329e07d7ae
Zach <zach.blizzard@one.verizon.com>
2018-05-09 12:21:38 -0500

    making tester a function again, i kind of like it more

--- src/components/container/Tester.js
+++ src/components/container/Tester.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MyContext from '../../MyContext';
-class Tester extends Component {
-    render() {
-        return (
-            <MyContext.Consumer>
-                {
-                    ({ name, changeName }) => (
 
-                        <div>
-                            { name }<br/>
-                            <input style={{ marginTop: 10 }}
-                                type="text" onChange={changeName} />
-                        </div>
-                    )
-                }
-            </MyContext.Consumer>
-        )
-    }
+export default function Tester(props) {
+    return (
+        <MyContext.Consumer>
+        { 
+            ({name, changeName}) => (
+                <div>
+                    { name }<br />
+                    <input style={{ marginTop: 10 }}
+                        onChange={ changeName } />
+                </div>
+            )
+        }
+        </MyContext.Consumer>
+    )
 }
-
-export default Tester;
