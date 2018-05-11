624a6cb2d1d813d51a6a4c84f721f23b78be6478
Zach <zach.blizzard@one.verizon.com>
2018-05-11 13:14:32 -0500

    only showing last 10

--- src/components/container/CETCheck.js
+++ src/components/container/CETCheck.js
@@ -30,21 +30,27 @@ export default class CETCheck extends Component {
                         let inProgressData = Object.assign([], this.state.inProgressData)
                         let labels = Object.assign([], this.state.labels)
                         
-                        let tmpProcessed = resp.data.filter(info => { return info.eocStatus.toLowerCase() === "processed" }).map(info => { return info.count })
+                        let tmpProcessed = resp.data.filter(info => { 
+                            return info.eocStatus.toLowerCase() === "processed" 
+                        }).map(info => { return info.count })
                         if (tmpProcessed.length > 0) {
                             processedData.push(tmpProcessed)
                         } else {
                             processedData.push(0)
                         }
 
-                        let tmpNotProcessed = resp.data.filter(info => { return info.eocStatus.toLowerCase() === "not_processed" }).map(info => { return info.count })
+                        let tmpNotProcessed = resp.data.filter(info => { 
+                            return info.eocStatus.toLowerCase() === "not_processed" 
+                        }).map(info => { return info.count })
                         if (tmpNotProcessed.length > 0) {
                             notProcessedData.push(tmpNotProcessed)
                         } else {
                             notProcessedData.push(0)
                         }
 
-                        let tmpInProgress = resp.data.filter(info => { return info.eocStatus.toLowerCase() === "in_progress" }).map(info => { return info.count })
+                        let tmpInProgress = resp.data.filter(info => { 
+                            return info.eocStatus.toLowerCase() === "in_progress" 
+                        }).map(info => { return info.count })
                         if (tmpInProgress.length > 0) {
                             inProgressData.push(tmpInProgress)
                         } else {
@@ -58,30 +64,21 @@ export default class CETCheck extends Component {
                                 labels: labels.slice(labels.length - 10, labels.length - 1),
                                 datasets: [{
                                     label: 'processed',
-                                    fillColor: "rgba(0,238,76,0.2)",
-                                    strokeColor: "rgba(0,238,76,1)",
+                                    backgroundColor: "rgba(0,238,76,0.2)",
+                                    borderColor: "rgba(0,238,76,1)",
                                     pointColor: "rgba(0,238,76,1)",
-                                    pointStrokeColor: "#00EE4C",
-                                    pointHighlightFill: "#00EE4C",
-                                    pointHighlightStroke: "rgba(0,238,76,1)",
                                     data: processedData.slice(processedData.length - 10, processedData.length - 1)
                                 }, {
                                     label: 'not processed',
-                                    fillColor: "rgba(255,82,82,0.2)",
-                                    strokeColor: "rgba(255,82,82,1)",
+                                    backgroundColor: "rgba(255,82,82,0.2)",
+                                    borderColor: "rgba(255,82,82,1)",
                                     pointColor: "rgba(255,82,82,1)",
-                                    pointStrokeColor: "#ff5252",
-                                    pointHighlightFill: "#ff5252",
-                                    pointHighlightStroke: "rgba(255,82,82,1)",
                                     data: notProcessedData.slice(notProcessedData.length - 10, notProcessedData.length - 1)
                                 }, {
                                     label: 'in progress',
-                                    fillColor: "rgba(255,180,0,0.2)",
-                                    strokeColor: "rgba(255,180,0,1)",
+                                    backgroundColor: "rgba(255,180,0,0.2)",
+                                    borderColor: "rgba(255,180,0,1)",
                                     pointColor: "rgba(255,180,0,1)",
-                                    pointStrokeColor: "#FFB400",
-                                    pointHighlightFill: "#FFB400",
-                                    pointHighlightStroke: "rgba(255,180,0,1)",
                                     data: inProgressData.slice(inProgressData.length - 10, inProgressData.length - 1)
                                 }]
                             },
