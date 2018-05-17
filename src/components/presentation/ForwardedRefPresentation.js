dc5ccb5d37cc8db999718d0b31e0929c891184ca
jimmyfargo <zblizzard@live.com>
2018-05-17 16:33:53 -0500

    adding a comment

--- src/components/presentation/ForwardedRefPresentation.js
+++ src/components/presentation/ForwardedRefPresentation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const ForwardedRefPresentation = React.forwardRef((props, ref) => {
+    // ref comes from the parent, and can be used there
     return (
         <div>
             <input type="text"
