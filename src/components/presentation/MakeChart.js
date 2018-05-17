71855635b21153c51c824c1f89182385a89d8378
jimmyfargo <zblizzard@live.com>
2018-05-17 15:35:35 -0500

    adding proptypes to makechart

--- src/components/presentation/MakeChart.js
+++ src/components/presentation/MakeChart.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Line } from "react-chartjs-2"
+import PropTypes from 'prop-types'
 
 const MakeChart = ({ chartData }) => {
     return (
         <Line data={ chartData } width="600" height="250" />
     )
+}
+
+MakeChart.propTypes = {
+    chartData: PropTypes.object.isRequired
 }
 
 export default MakeChart
